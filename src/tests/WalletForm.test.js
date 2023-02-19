0c74f051b6afadac21511827b177380c50332dde
claytongom <claytoncnx@gmail.com>
2023-02-19 13:34:47 -0300

    resolução requisito 5 - testes em progresso_4

--- src/tests/WalletForm.test.js
+++ src/tests/WalletForm.test.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { renderWithRouterAndRedux } from './helpers/renderWith';
-import WalletForm from '../components/WalletForm';
+import Wallet from '../pages/Wallet';
+import mockData from './helpers/mockData';
 
-describe('Verifica pagaina WalletForm', () => {
-  it('Testa se os titulos dos campos são renderizados ', () => {
-    renderWithRouterAndRedux(<WalletForm />);
+describe('Verifica a pagina Wallet', () => {
+  beforeEach(() => {
+    jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve({ json: () => Promise.resolve(mockData),
+    }));
+  });
 
-    const valueExpense = screen.getByText('Valor da despesa:');
-    const descriptionExpense = screen.getByText('Descrição da despesa:');
-    const currencyExpense = screen.getByText('Moeda:');
-    const methodExpense = screen.getByText('Método de pagamento:');
-    const tagExpense = screen.getByText('Tag:');
-    const btnAddExpense = screen.getByText('Adicionar despesa');
-    expect(valueExpense).toBeInTheDocument();
-    expect(descriptionExpense).toBeInTheDocument();
-    expect(currencyExpense).toBeInTheDocument();
-    expect(methodExpense).toBeInTheDocument();
-    expect(tagExpense).toBeInTheDocument();
-    expect(btnAddExpense).toBeInTheDocument();
+  it('Verifica se os campos estão na tela', async () => {
+    renderWithRouterAndRedux(<Wallet />);
+
+    const value = screen.getByTestId('value-input');
+    const currency = screen.getByTestId('currency-input');
+    const method = screen.getByTestId('method-input');
+    const tag = screen.getByTestId('tag-input');
+    const description = screen.getByTestId('description-input');
+    const button = screen.getByText('Adicionar despesa');
+    expect(value).toBeInTheDocument();
+    expect(currency).toBeInTheDocument();
+    expect(method).toBeInTheDocument();
+    expect(tag).toBeInTheDocument();
+    expect(description).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+    userEvent.click(value);
+    userEvent.click(currency);
+    userEvent.click(method);
+    userEvent.click(tag);
+    userEvent.click(description);
+    userEvent.click(button);
   });
 });
